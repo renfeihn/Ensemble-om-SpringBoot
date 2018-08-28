3769da0d6a1ce1b296e8ebc929a9c088b20ef884
jiajt <jiajt@dcits.com>
2018-08-28 10:17:38 +0800

    用户登录连通核心

---
+++ src/main/java/com/dcits/ensembleom/model/dbmodel/system/WebUserClass.java
@@ -0,0 +1,14 @@
+package com.dcits.ensembleom.model.dbmodel.system;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * Created by jiajt on 2018/8/27.
+ */
+@Getter
+@Setter
+public class WebUserClass implements Serializable {
+}
