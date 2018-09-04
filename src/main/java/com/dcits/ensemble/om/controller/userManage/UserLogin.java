0e10d1ed6d549a4afa58d0de4c7575a5a34658d4
chengliang <chengliang@dcits.com>
2018-09-04 16:29:26 +0800

    集成shiro，控制安全权限

--- src/main/java/com/dcits/ensemble/om/controller/userManage/UserLogin.java
+++ src/main/java/com/dcits/ensemble/om/controller/userManage/UserLogin.java
@@ -1,11 +1,18 @@
 package com.dcits.ensemble.om.controller.userManage;
 
 import com.alibaba.fastjson.JSON;
+import com.dcits.ensemble.om.intercept.LoginIntercept;
 import com.dcits.ensemble.om.model.RequestBean;
 import com.dcits.ensemble.om.model.dbmodel.system.WebUser;
 import com.dcits.ensemble.om.repository.userManage.WebUserRespository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.crypto.hash.Md5Hash;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,21 +24,32 @@ import java.util.Map;
 /**
  * Created by jiajt on 2018/8/27.
  */
-@Api(value = "/userLoginCheck", tags = "用户模块")
+@Api(value = "/login", tags = "用户模块")
 @Controller
 public class UserLogin {
+    private static final Logger logger = LoggerFactory.getLogger(UserLogin.class);
     @Resource
     private WebUserRespository webUserRespository;
 
     @ApiOperation(value = "用户登录验证", notes = "用户登录验证")
-    @RequestMapping("/userLoginCheck")
+    @RequestMapping("/login")
     @CrossOrigin
     @ResponseBody
-    public String getUserMsgByUserIs(HttpServletResponse response, @RequestBody RequestBean requestBean) {
+    public String getUserMsgByUserIs(HttpServletResponse response, @RequestParam(value = "userName", required = false) String userName) {
+
+        if (userName == null) {
+            return "请重新登录！";
+        }
+
         response.setHeader("Content-Type", "application/json;charset=UTF-8");
-        Map map = requestBean.getBody();
-        String userId = map.get("username").toString();
-        List<WebUser> webUser = webUserRespository.findByUserId(userId);
+        List<WebUser> webUser = webUserRespository.findByUserId(userName);
+        WebUser user = webUser.get(0);
+        logger.info(webUser.toString());
+        Subject currentUser = SecurityUtils.getSubject();
+        logger.info(new Md5Hash(user.getPassword(), userName).toHex());
+        //String pwd =new Md5Hash(user.getPassword(), user.getUserId()).toHex();
+        UsernamePasswordToken token = new UsernamePasswordToken(user.getUserId(), user.getPassword());
+        currentUser.login(token);
         return JSON.toJSONString(webUser);
     }
 }
--- src/main/java/com/dcits/ensemble/om/service/userManage/WebUserService.java
+++ src/main/java/com/dcits/ensemble/om/service/userManage/WebUserService.java
@@ -1,6 +1,8 @@
 package com.dcits.ensemble.om.service.userManage;
 
+import com.dcits.ensemble.om.model.dbmodel.system.WebUser;
 import com.dcits.ensemble.om.repository.userManage.WebUserRespository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,11 +10,15 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class WebUserService {
-//    @Autowired
+    @Autowired
     WebUserRespository webUserRespository;
 
-    public boolean getUserMsgByUserId(String userId,String password){
+    public boolean getUserMsgByUserId(String userId, String password) {
 //        WebUser webUser = webUserRespository.getUserMsgByUserId(userId,password);
         return userId.equals("admin") && password.equals("123456");
+    }
+
+    public WebUser findByUsername(String username) {
+        return webUserRespository.findByUserId(username).get(0);
     }
 }
