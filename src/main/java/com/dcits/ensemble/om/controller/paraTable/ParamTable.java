ad30e59e83f1935ba67f0c496770a095fa075efb
ligana <ligan@dcits.com>
2018-11-08 15:10:39 +0800

    修改名字

---
+++ src/main/java/com/dcits/ensemble/om/controller/paraTable/ParamTable.java
@@ -0,0 +1,49 @@
+package com.dcits.ensemble.om.controller.paraTable;
+
+import com.dcits.ensemble.om.controller.model.Result;
+import com.dcits.ensemble.om.controller.model.ResultUtils;
+import com.dcits.ensemble.om.model.dbmodel.OmProcessMainFlow;
+import com.dcits.ensemble.om.model.dbmodel.OmProcessRecordHist;
+import com.dcits.ensemble.om.model.prodFactory.MbProdInfo;
+import com.dcits.ensemble.om.repository.paraFlow.OmProcessDetailHistRepository;
+import com.dcits.ensemble.om.repository.paraFlow.OmProcessMainFlowRepository;
+import com.dcits.ensemble.om.repository.paraFlow.OmProcessRecordHistRepository;
+import com.dcits.ensemble.om.service.paraFlow.DifferenceInfo;
+import com.dcits.ensemble.om.service.paraFlow.FlowManagement;
+import com.dcits.ensemble.om.service.paraTable.ParamTableService;
+import com.dcits.ensemble.om.service.prodFactory.MbProdInfoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Api(value = "/getParamTable", tags = "单表模块")
+@Controller
+public class ParamTable {
+    private ParamTableService paramTableService;
+    @ApiOperation(value = "单表信息", notes = "获取单表信息")
+    @RequestMapping("/getTableInfo")
+    @ResponseBody
+    public Result getProdInfo(HttpServletResponse response, @RequestParam(value = "tableName", required = false) String tableName) {
+        //Map map = requestBean.getBody();
+        response.setHeader("Content-Type", "application/json;charset=UTF-8");
+        Map responseMap = new HashMap<>();
+        //1.根据表名获取参数列信息
+        responseMap.put("column",paramTableService.getTableColumn(tableName));
+        responseMap.put("columnInfo",paramTableService.getTableInfo(tableName));
+        //2.根据参数列信息及表名差数据库获取数据信息
+        //3.将得到的信息分为两个对象集合传到前端
+        return ResultUtils.success();
+    }
+
+}
