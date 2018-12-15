606cd68f6b35892911b793983b0d166e4cdf4887
jiajt <jiajt@dcits.com>
2018-12-15 11:11:44 +0800

    产品展示界面脚本

---
+++ db/patch/20181215_param.sql
@@ -0,0 +1,2 @@
+INSERT INTO `ensemble`.`om_menu` (`menu_seq_no`, `menu_id`, `menu_name`, `menu_parent_id`, `menu_level`, `menu_status`, `menu_icon`, `menu_component`, `menu_title`) VALUES ('19', '14', 'prodWindow', '3', '2', 'A', 'widgets', 'prodWindow', '产品展示');
+INSERT INTO `ensemble`.`om_menu_role` (`role_id`, `menu_id`) VALUES ('1', '14');
