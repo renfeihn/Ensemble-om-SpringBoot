8ffa12162fc1e8791b342b6200968002610bbfed
jiajt <jiajt@dcits.com>
2019-03-11 10:34:57 +0800

    flowable工作流引入

---
+++ src/main/java/com/dcits/ensemble/om/flowable/ManagerTaskHandler.java
@@ -0,0 +1,11 @@
+package com.dcits.ensemble.om.flowable;
+
+import org.flowable.engine.delegate.TaskListener;
+import org.flowable.task.service.delegate.DelegateTask;
+
+public class ManagerTaskHandler implements TaskListener {
+    @Override
+    public void notify(DelegateTask delegateTask) {
+        delegateTask.setAssignee("经理");
+    }
+}
